6809b280aee1649d8926e015e81611dfe1005298
k8xu <38595657+k8xu@users.noreply.github.com>
2018-10-12 12:15:58 -0400

    add route function to app.py

--- gender_novels/deployment/app.py
+++ gender_novels/deployment/app.py
@@ -9,32 +9,21 @@ print(app.config)
 
 
 @app.route('/')
-def render_index():
-    return render_template('gender_novels.html')
-
-@app.route('/')
-def md_convert():
-    """
-    Converts file written in markdown to html
-    TODO (Xu): Find or create method
-    """
+def render_gender_novels():
+    return render_template('landing_page.html')
 
 
-@app.route('/team/')
-def render_team():
+@app.route('/<string:text>/')
+def render_base_text(text):
     """
-    Renders the team page
-    TODO (Backend Developer): Implement this route
-    """
-    return render_template('team.html')
+    Renders subpage with base.html template that describes our gender novels research
+    :param text: placeholder in base.html written as {{ text }} for words that will vary based on subpage
+    :return: subpage with {{ text }} substituted with actual text such as the About page
 
-@app.route('/projects/')
-def render_projects():
+    TODO (Xu): Figure out how to link other html files to base.html
     """
-    Renders the projects page
-    TODO (Xu): Implement this route with list of projects
-    """
-    return "Check out our Projects!"
+    return render_template('base.html', text = text)
+
 
 if __name__ == '__main__':
     # open a webbrowser on the landing page
--- gender_novels/deployment/using_flask.md
+++ gender_novels/deployment/using_flask.md
@@ -1,2 +1,5 @@
 # Using Flask
 [Helpful Flask tutorial](http://www.tutorialspoint.com/flask)
+
+### Kate's notes: 
+[Google Doc - still a work in progress](https://goo.gl/YRrbwm)
