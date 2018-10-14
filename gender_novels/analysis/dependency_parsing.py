f0b17876ed80a58c614e173130ad7d0ff4b589d5
varmichelle <limichelle.lm@gmail.com>
2018-10-14 16:30:08 -0400

    print dependency triples of novels

--- gender_novels/analysis/dependency_parsing.py
+++ gender_novels/analysis/dependency_parsing.py
@@ -3,8 +3,9 @@ This file is intended for dependency parsing side tests.
 """
 
 import urllib
-
 from nltk.parse.stanford import StanfordDependencyParser
+from gender_novels.corpus import Corpus
+from nltk.tokenize import sent_tokenize
 
 path_to_jar = "assets/stanford-parser.jar"
 path_to_models_jar = "assets/stanford-parser-3.9.1-models.jar"
@@ -17,32 +18,26 @@ urllib.request.urlretrieve(url_to_models_jar, path_to_models_jar)
 
 dependency_parser = StanfordDependencyParser(path_to_jar, path_to_models_jar)
 
-"""
-Parse multiple sentences at a time
-"""
+novels = Corpus('sample_novels').novels
 
-sentences = ["The quick brown fox jumped over the lazy dog",
-             "He walked with her",
-             "He walked by her",
-             "I'm not going to tell you about her",
-             "She has two rooms",
-             "She was brave",
-             "He hit her"]
+for novel in novels:
+    sentences = sent_tokenize(novel.text)
+    print(sentences)
 
-result2 = dependency_parser.raw_parse_sents(sentences)
+    result = dependency_parser.raw_parse_sents(sentences)
 
-for sentence in result2:
-    parse = next(sentence)
-    # dependency tree
-    print("------DEPENDENCY TREE------")
-    print(parse.tree())
-    # CoNLL format
-    print("------CONLL FORMAT------")
-    print(parse.to_conll(4))
-    # dependency triples of the form ((head word, head tag), rel, (dep word, dep tag))
-    # link defining dependencies: https://nlp.stanford.edu/software/dependencies_manual.pdf
-    print("------DEPENDENCY TRIPLES------")
-    triples = list(parse.triples())
-    for triple in triples:
-        print(triple)
-    print()
+    for sentence in result:
+        parse = next(sentence)
+        # dependency tree
+        # print("------DEPENDENCY TREE------")
+        # print(parse.tree())
+        # CoNLL format
+        # print("------CONLL FORMAT------")
+        # print(parse.to_conll(4))
+        # dependency triples of the form ((head word, head tag), rel, (dep word, dep tag))
+        # link defining dependencies: https://nlp.stanford.edu/software/dependencies_manual.pdf
+        print("------DEPENDENCY TRIPLES------")
+        triples = list(parse.triples())
+        for triple in triples:
+            print(triple)
+        print()
