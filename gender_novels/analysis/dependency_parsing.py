e6a2f8a73893c7c7bb84f1bb974b7a0346943af6
varmichelle <limichelle.lm@gmail.com>
2018-10-14 16:45:22 -0400

    structure dependency_parsing.py as oop

--- gender_novels/analysis/dependency_parsing.py
+++ gender_novels/analysis/dependency_parsing.py
@@ -1,26 +1,24 @@
-"""
-This file is intended for dependency parsing side tests.
-"""
-
 import urllib
 from nltk.parse.stanford import StanfordDependencyParser
 from gender_novels.corpus import Corpus
 from nltk.tokenize import sent_tokenize
 
-path_to_jar = "assets/stanford-parser.jar"
-path_to_models_jar = "assets/stanford-parser-3.9.1-models.jar"
+if __name__ == "__main__":
 
-# The jar files are too big to commit directly, so download them
-url_to_jar = "http://www.trecento.com/dh_lab/nltk_jar/stanford-parser.jar"
-url_to_models_jar = "http://www.trecento.com/dh_lab/nltk_jar/stanford-parser-3.9.1-models.jar"
-urllib.request.urlretrieve(url_to_jar, path_to_jar)
-urllib.request.urlretrieve(url_to_models_jar, path_to_models_jar)
+    path_to_jar = "assets/stanford-parser.jar"
+    path_to_models_jar = "assets/stanford-parser-3.9.1-models.jar"
 
-dependency_parser = StanfordDependencyParser(path_to_jar, path_to_models_jar)
+    # The jar files are too big to commit directly, so download them
+    url_to_jar = "http://www.trecento.com/dh_lab/nltk_jar/stanford-parser.jar"
+    url_to_models_jar = "http://www.trecento.com/dh_lab/nltk_jar/stanford-parser-3.9.1-models.jar"
+    urllib.request.urlretrieve(url_to_jar, path_to_jar)
+    urllib.request.urlretrieve(url_to_models_jar, path_to_models_jar)
 
-novels = Corpus('sample_novels').novels
+    dependency_parser = StanfordDependencyParser(path_to_jar, path_to_models_jar)
 
-for novel in novels:
+    novels = Corpus('sample_novels').novels
+
+    novel = novels[0]
     sentences = sent_tokenize(novel.text)
     print(sentences)
 
