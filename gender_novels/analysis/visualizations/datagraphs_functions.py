9bcf2bda4a72df84bc01a38a260da730ae213fdb
felixtran39 <43281010+felixtran39@users.noreply.github.com>
2018-10-19 15:36:33 -0400

    Capitalize title of graph without changing its filename when saved. Created function that calls all plot functions and that function is called under the if __name__ statement

--- gender_novels/analysis/visualizations/datagraphs_functions.py
+++ gender_novels/analysis/visualizations/datagraphs_functions.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from gender_novels.corpus import Corpus
 
-def plt_pubyears(years,name_of_data):
+def plt_pubyears(years,corpus_name):
     '''
     Creates a histogram displaying the frequency of books that were published within a 20 year 
     period
@@ -15,16 +15,16 @@ def plt_pubyears(years,name_of_data):
     plt.hist(years,bins,histtype='bar',rwidth=.8,color='plum')
     plt.xlabel('Year', size=13,weight='bold',color='slategray')
     plt.ylabel('Frequency',size=13,weight='bold',color='slategray')
-    plt.title('Publication Year Concentration for '+name_of_data,size=15,weight='bold',
+    plt.title('Publication Year Concentration for '+corpus_name.title(),size=15,weight='bold',
               color='slategray')
     plt.yticks(size=13,color='slategray')
     plt.xticks([i for i in range(min(years),max(years)+9,10)],size=13,color='slategray')
     for label in ax1.xaxis.get_ticklabels():
         label.set_rotation(60)
     plt.subplots_adjust(left=.1,bottom=.18,right=.95,top=.9)
-    plt.savefig('date_of_pub_for_'+name_of_data+'.png')
+    plt.savefig('date_of_pub_for_'+corpus_name+'.png')
 
-def plt_pubcountries(pub_country,name_of_data):
+def plt_pubcountries(pub_country,corpus_name):
     '''
     Creates a bar graph displaying the frequency of books that were published in each country
     :param pub_country: list
@@ -42,13 +42,14 @@ def plt_pubcountries(pub_country,name_of_data):
     plt.bar(x,y,color='plum')
     plt.xlabel('Countries',size=13,weight='bold',color='slategray')
     plt.ylabel('Frequency',size=13,weight='bold',color='slategray')
-    plt.title('Country of Publication for '+name_of_data,size=15,color='slategray',weight='bold')
+    plt.title('Country of Publication for '+corpus_name.title(),size=15,color='slategray',
+              weight='bold')
     plt.xticks(color='slategray',size=13)
     plt.yticks(color='slategray',size=13)
     plt.subplots_adjust(left=.1,bottom=.18,right=.95,top=.9)
-    plt.savefig('country_of_pub_for_'+name_of_data+'.png')
+    plt.savefig('country_of_pub_for_'+corpus_name+'.png')
 
-def plt_gender_breakdown(pub_gender,name_of_data):
+def plt_gender_breakdown(pub_gender,corpus_name):
     '''
     Creates a pie chart displaying the composition of male and female writers in the data
     :param pub_gender: list
@@ -65,24 +66,31 @@ def plt_gender_breakdown(pub_gender,name_of_data):
     genders=[i for i in gendercount]
     labelgenders=[]
     for i in range(len(genders)):
-        labelgenders.append(genders[i]+': ' + str(round(slices[i],2)*100)+'%')
+        labelgenders.append((genders[i]+': ' + str(round(slices[i],2)*100)+'%').title())
     colors=['slateblue','mediumpurple','plum']
     plt.pie(slices,colors=colors,labels=labelgenders,textprops={'fontsize':15})
-    plt.title('Gender Breakdown for '+name_of_data,size=15,color='slategray',weight='bold')
+    plt.title('Gender Breakdown for '+corpus_name.title(),size=15,color='slategray',weight='bold')
     plt.legend()
     plt.subplots_adjust(left=.1,bottom=.1,right=.9,top=.9)
-    plt.savefig('gender_breakdown_for_'+name_of_data+'.png')
+    plt.savefig('gender_breakdown_for_'+corpus_name+'.png')
 
-if __name__=='__main__':
+def create_corpus_summary_visualizations(corpus_name):
+    '''
+    Runs through all plt functions given a corpus name
+    :param corpus_name: str
+    '''
     pubyears=[]
     pubgender=[]
     pubcountry=[]
-    c = Corpus('sample_novels')
+    c = Corpus(corpus_name)
     for novel in c.novels:
         pubyears.append(novel.date)
         pubgender.append(novel.author_gender)
         pubcountry.append(novel.country_publication)
-    plt_gender_breakdown(pubgender, 'sample novels')
-    plt_pubyears(pubyears,'sample novels')
-    plt_pubcountries(pubcountry,'sample novels')
+    corpus_name=corpus_name.replace('_',' ')
+    plt_gender_breakdown(pubgender, corpus_name)
+    plt_pubyears(pubyears,corpus_name)
+    plt_pubcountries(pubcountry,corpus_name)
 
+if __name__=='__main__':
+    create_corpus_summary_visualizations('sample_novels')
