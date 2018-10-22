8ecf5d6886004db0b9bb7a586a597b146875942b
varmichelle <limichelle.lm@gmail.com>
2018-10-22 15:18:40 -0400

    documentation for dependency_parsing.py

--- gender_novels/analysis/dependency_parsing.py
+++ gender_novels/analysis/dependency_parsing.py
@@ -20,45 +20,54 @@ def load_jars(path_to_jar, path_to_models_jar):
         urllib.request.urlretrieve(url_to_models_jar, path_to_models_jar)
 
 
-def count_tag(pronoun, tag, tree):
-    count = 0
-    for triple in tree:
-        if triple[1] == tag and triple[2][0] == pronoun:
-            count += 1
-    return count
-
-
-def create_tree(triples):
-    tree = []
-    for triple in triples:
-        tree.append(triple)
-    return tree
-
-def print_tree(tree):
-    for triple in tree:
-        print(triple)
-
-
 def count_gender_subj_obj(tree):
-    male_subj_count = count_tag("he", "nsubj", tree)
-    male_obj_count = count_tag("him", "dobj", tree)
-    female_subj_count = count_tag("she", "nsubj", tree)
-    female_obj_count = count_tag("her", "dobj", tree)
+    """
+    This function takes in an dependency tree for a sentence and counts female and male subject and
+    object occurences
+
+    We have chosen not to include indirect object positions because whether or not they represent
+    passivity (at least, compared to being a direct object) is debatable
+
+    :param tree: An array which is a dependency tree
+    :return: the counts of male and female subject and object occurences as a tuple of 4
+    """
+
+    male_subj_count = male_obj_count = female_subj_count = female_obj_count = 0
+    for triple in tree:
+        if triple[1] == "nsubj" and triple[2][0] == "he":
+            male_subj_count += 1
+        if triple[1] == "dobj" and triple[2][0] == "him":
+            male_obj_count += 1
+        if triple[1] == "nsubj" and triple[2][0] == "she":
+            female_subj_count += 1
+        if triple[1] == "dobj" and triple[2][0] == "her":
+            female_subj_count += 1
 
     return (male_subj_count, male_obj_count, female_subj_count, female_obj_count)
 
 
 def parse_sentence(sentence):
+    """
+    This function does all sentence parsing (we cannot split this up into separate functions for
+    performance reasons (each additional function will require iterating over the entire tree again)
+    :param sentence: sentence (string) we want to parse
+    :return: the counts of male and female subject and object occurrences as a tuple of 4
+    """
     result = dependency_parser.raw_parse(sentence.lower())
     parse = next(result)
     # dependency triples of the form ((head word, head tag), rel, (dep word, dep tag))
     # link defining dependencies: https://nlp.stanford.edu/software/dependencies_manual.pdf
     triples = list(parse.triples())
-    tree = create_tree(triples)
+    tree = list(triples)
     return count_gender_subj_obj(tree)
 
 
 def parse_novel(novel):
+    """
+    This function calls the parse_sentence function for all sentences in the novel
+    :param novel: Novel object we want to analyze
+    :return: the counts of male and female subject and object occurrences as a tuple of 4
+    """
     sentences = sent_tokenize(novel.text.replace("\n", " "))
     t_male_subj_count = t_male_obj_count = t_female_subj_count = t_female_obj_count = 0
     for sentence in sentences:
@@ -83,29 +92,19 @@ if __name__ == "__main__":
 
     novels = Corpus('sample_novels').novels
     novel = novels[0]
-    print(parse_novel(novel))
+    # print(parse_novel(novel))
 
-    # sentences = {"She told him to pass the ball",
-    #              "He told something to her",
-    #              "He gave her a gift",
-    #              "The red dog sang a purple song for the blue cat",
-    #              "The girl said that the man believes that the woman walked"}
-    #
-    # for sentence in sentences:
-    #     result = dependency_parser.raw_parse(sentence.lower())
-    #     parse = next(result)
-    #     # dependency triples of the form ((head word, head tag), rel, (dep word, dep tag))
-    #     # link defining dependencies: https://nlp.stanford.edu/software/dependencies_manual.pdf
-    #     triples = list(parse.triples())
-    #     tree = create_tree(triples)
-    #
-    #     # print(count_tag("she", "nsubj", tree))
-    #
-    #     print(sentence)
-    #     print("--")
-    #     print(parse.tree())
-    #     print("--")
-    #     print_tree(tree)
-    #     print("--")
-    #     print(count_gender_subj_obj(tree))
-    #     print()
+    sentences = {"She hit him before he could hit her back"}
+
+    for sentence in sentences:
+        result = dependency_parser.raw_parse(sentence.lower())
+        parse = next(result)
+        # dependency triples of the form ((head word, head tag), rel, (dep word, dep tag))
+        # link defining dependencies: https://nlp.stanford.edu/software/dependencies_manual.pdf
+        tree = list(parse.triples())
+        print(sentence)
+        print("--")
+        print(parse.tree())
+        print("--")
+        print(count_gender_subj_obj(tree))
+        print()
