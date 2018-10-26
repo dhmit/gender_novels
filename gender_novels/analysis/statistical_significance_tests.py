d192cdffdc624bde28ceea49d572a6762421b8ae
Sophia Zhi <sophiazhi@gmail.com>
2018-10-26 16:09:24 -0400

    stats functions -- added doctests and "if name == main"

--- gender_novels/analysis/statistical_significance_tests.py
+++ gender_novels/analysis/statistical_significance_tests.py
@@ -15,6 +15,14 @@ def ind_ttest(array1, array2, pvalue_target=0.05):
     by men
     :param pvalue_target: largest p-value for which we consider the test statistically significant
     :return: True if the difference in the means of the two arrays are significant, False otherwise
+    >>> a1 = np.array([1, 2, 3, 4, 5])
+    >>> a2 = np.array([1, 2, 3, 4, 5])
+    >>> ind_ttest(a1, a2)
+    False
+    >>> a3 = np.array([3, 4, 8, 6, 2])
+    >>> a4 = np.array([14, 8, 17, 9, 16])
+    >>> ind_ttest(a3, a4)
+    True
     '''
 
     # don't assume that the two variables have equal standard deviation
@@ -33,7 +41,16 @@ def linear_regression(array1, array2, pvalue_target=0.05):
     :param pvalue_target: largest p-value for which we consider the correlation statistically
     significant
     :return: True if the correlation is significant, False otherwise
+    >>> a1 = np.array([1, 2, 3, 4, 5])
+    >>> a2 = np.array([1, 2, 3, 4, 5])
+    >>> linear_regression(a1, a2)
+    True
+    >>> a3 = np.array([3, 4, 8, 6, 2])
+    >>> a4 = np.array([14, 8, 17, 9, 16])
+    >>> linear_regression(a3, a4)
+    False
     '''
+
     pvalue = stats.linregress(array1, array2)[3]
     return pvalue < pvalue_target
 
@@ -47,6 +64,14 @@ def pearson_correlation(array1, array2, pvalue_target=0.05):
     :param pvalue_target: largest p-value for which we consider the correlation statistically
     significant
     :return: True if the correlation is significant, False otherwise
+    >>> a1 = np.array([1, 2, 3, 4, 5])
+    >>> a2 = np.array([1, 2, 3, 4, 5])
+    >>> pearson_correlation(a1, a2)
+    True
+    >>> a3 = np.array([3, 4, 8, 6, 2])
+    >>> a4 = np.array([14, 8, 17, 9, 16])
+    >>> pearson_correlation(a3, a4)
+    False
     '''
 
     pvalue = stats.pearsonr(array1, array2)[1]
@@ -54,71 +79,72 @@ def pearson_correlation(array1, array2, pvalue_target=0.05):
     return pvalue < pvalue_target
 
 
-'''
-independent variables:
-    author gender
-    year of publication
-    character gender
-    country of publication
+if __name__ == "__main__":
+    '''
+    Finds the minimum p-value to deem the relationship between metadata variables and analysis 
+    results significant
+    
+    Independent variables (metadata) include:
+        author gender
+        year of publication
+        country of publication
+    
+    Dependent variables:
+        distance between 'he' and 'she'
+        the frequency of gendered pronouns used as subjects or objects
+    '''
 
-dependent variables:
-    distance between "he" and "she"
-    subject vs object pronouns
-
-use subject_pronouns_gender_comparison(corp, subject_gender)
-'''
-
-corp = Corpus('test_corpus')
-# corp = Corpus('gutenberg')
-# corp = Corpus('sample_novels')
-subject_female_pronoun_dict = gender_pronoun_freq_analysis.subject_pronouns_gender_comparison(corp,
+    corp = Corpus('test_corpus')
+    # corp = Corpus('gutenberg')
+    # corp = Corpus('sample_novels')
+    subject_female_pronoun_dict = gender_pronoun_freq_analysis.subject_pronouns_gender_comparison(corp,
                                                                                               'female')
+    # create lists for novels, publication date, etc. with all entries in the same
+    # corresponding order
+    novel_list = []
+    novel_year_list = []
+    novel_author_gender_list = []
+    subject_female_pronoun_list = []
 
-# create ordered list of novels
-novel_list = []
-novel_year_list = []
-novel_author_gender_list = []
-subject_female_pronoun_list = []
+    for novel in corp:
+        novel_list.append(novel)
+        novel_year_list.append(novel.date)
+        novel_author_gender_list.append(novel.author_gender)
+        subject_female_pronoun_list.append(subject_female_pronoun_dict[novel])
 
-for novel in corp:
-    novel_list.append(novel)
-    novel_year_list.append(novel.date)
-    novel_author_gender_list.append(novel.author_gender)
-    subject_female_pronoun_list.append(subject_female_pronoun_dict[novel])
+    # split the subject_female_pronoun_list into a list for male authors and a list for female authors
+    # used for independent t-test
+    male_subject_pronoun_list = []
+    female_subject_pronoun_list = []
+    for novel in novel_list:
+        if novel.author_gender == 'male':
+            male_subject_pronoun_list.append(subject_female_pronoun_dict[novel])
+        else:
+            female_subject_pronoun_list.append(subject_female_pronoun_dict[novel])
 
-# split the subject_female_pronoun_list into a list for male authors and a list for female authors
-# used for independent t-test
-male_subject_pronoun_list = []
-female_subject_pronoun_list = []
-for novel in novel_list:
-    if novel.author_gender == 'male':
-        male_subject_pronoun_list.append(subject_female_pronoun_dict[novel])
-    else:
-        female_subject_pronoun_list.append(subject_female_pronoun_dict[novel])
-
-# test if the difference in frequency of female pronouns in novels authored by men vs by women is
-# significant
-# by iterating over p-values to find the minimum p-value
-print("Independent t-test on frequency of female pronouns as subjects in male-authored vs "
+    # test if the difference in frequency of female pronouns in novels authored by men vs by women is
+    # significant
+    # by iterating over p-values to find the minimum p-value
+    print("Independent t-test on frequency of female pronouns as subjects in male-authored vs "
       "female-authored "
       "novels")
-for index in range(10):
-    pvalue = (index+1)*0.05
-    print("p-value target = " + str(round(pvalue, 2)) + ": " + str(ind_ttest(np.array(
+    for index in range(10):
+        pvalue = (index+1)*0.05
+        print("p-value target = " + str(round(pvalue, 2)) + ": " + str(ind_ttest(np.array(
         male_subject_pronoun_list), np.array(female_subject_pronoun_list),
                     pvalue)))
 
-# tests for the correlation between publication year and frequency of female pronouns as subjects
-print()
-print("Linear regression on publication year and frequency of female pronouns as subjects")
-for index in range(10):
-    pvalue = (index+1)*0.05
-    print("p-value target = " + str(round(pvalue, 2)) + ": " + str(linear_regression(np.array(
+    # tests for the correlation between publication year and frequency of female pronouns as subjects
+    print()
+    print("Linear regression on publication year and frequency of female pronouns as subjects")
+    for index in range(10):
+        pvalue = (index+1)*0.05
+        print("p-value target = " + str(round(pvalue, 2)) + ": " + str(linear_regression(np.array(
         novel_year_list), np.array(subject_female_pronoun_list), pvalue)))
 
-print()
-print("Pearson correlation on publication year and frequency of female pronouns as subjects")
-for index in range(10):
-    pvalue = (index+1)*0.05
-    print("p-value target = " + str(round(pvalue, 2)) + ": " + str(pearson_correlation(np.array(
+    print()
+    print("Pearson correlation on publication year and frequency of female pronouns as subjects")
+    for index in range(10):
+        pvalue = (index+1)*0.05
+        print("p-value target = " + str(round(pvalue, 2)) + ": " + str(pearson_correlation(np.array(
         novel_year_list), np.array(subject_female_pronoun_list), pvalue)))
