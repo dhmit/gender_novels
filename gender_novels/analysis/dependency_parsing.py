c2991c01712f4c6de238c29079df5efb2fb17674
varmichelle <limichelle.lm@gmail.com>
2018-10-26 23:58:45 -0400

    adjectives and verbs associated

--- gender_novels/analysis/dependency_parsing.py
+++ gender_novels/analysis/dependency_parsing.py
@@ -85,8 +85,37 @@ def parse_novel(novel, parser):
     # dependency triples of the form ((head word, head tag), rel, (dep word, dep tag))
     # link defining dependencies: https://nlp.stanford.edu/software/dependencies_manual.pdf
     tree = list(result)
-    counts = count_gender_subj_obj(tree)
-    return counts
+    # counts = count_gender_subj_obj(tree)
+    # return counts
+
+    male_subj_count = male_obj_count = female_subj_count = female_obj_count = 0
+    female_adjectives = []
+    male_adjectives = []
+    female_verbs = []
+    male_verbs = []
+    for sentence in tree:
+        for triple in next(sentence).triples():
+            if triple[1] == "nsubj" and triple[2][0] == "he":
+                male_subj_count += 1
+            if triple[1] == "dobj" and triple[2][0] == "him":
+                male_obj_count += 1
+            if triple[1] == "nsubj" and triple[2][0] == "she":
+                female_subj_count += 1
+            if triple[1] == "dobj" and triple[2][0] == "her":
+                female_obj_count += 1
+            if triple[1] == "nsubj" and triple[0][1] == "JJ":
+                if triple[2][0] == "she":
+                    female_adjectives.append(triple[0][0])
+                elif triple[2][0] == "he":
+                    male_adjectives.append(triple[0][0])
+            if triple[1] == "nsubj" and triple[0][1] == "VBD":
+                if triple[2][0] == "she":
+                    female_verbs.append(triple[0][0])
+                elif triple[2][0] == "he":
+                    male_verbs.append(triple[0][0])
+
+    return (male_subj_count, male_obj_count, female_subj_count, female_obj_count,
+            female_adjectives, male_adjectives, female_verbs, male_verbs)
 
 
 def test_analysis():
@@ -94,14 +123,24 @@ def test_analysis():
     This function contains all analysis code to be run (previously in main function)
     """
 
-    parser = get_parser("assets/stanford-parser.jar","assets/stanford-parser-3.9.1-models.jar")
+    # parser = get_parser("assets/stanford-parser.jar","assets/stanford-parser-3.9.1-models.jar")
+    #
+    # novels = Corpus('sample_novels').novels
+    # novel = novels[0]
+    # start = time.time()
+    # print(parse_novel(novel, parser))
+    # end = time.time()
+    # print(end-start)
 
+    parser = get_parser("assets/stanford-parser.jar",
+                             "assets/stanford-parser-3.9.1-models.jar")
     novels = Corpus('sample_novels').novels
-    novel = novels[0]
-    start = time.time()
-    print(parse_novel(novel, parser))
-    end = time.time()
-    print(end-start)
+    novel_metadata = {'author': 'Hawthorne, Nathaniel', 'title': 'Scarlet Letter', 'corpus_name': 'sample_novels',
+                      'date': '1900', 'filename': None, 'text': "He told her. She was blue as she hit the red man. She "
+                                                                "ate chicken and ran. He killed "
+                                                                "her. She was killed by him. "}
+    toy_novel = Novel(novel_metadata)
+    print(parse_novel(toy_novel, parser))
 
 
 if __name__ == "__main__":
