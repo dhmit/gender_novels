b045eab8aaf11e90ec8e6a1c39798fb82cd2caf3
Michael Scott Cuthbert <cuthbert@mit.edu>
2018-10-30 14:50:28 -0400

    Further simplifications to website -- everything in /markdowns/ renders

--- gender_novels/deployment/app.py
+++ gender_novels/deployment/app.py
@@ -15,13 +15,12 @@ landing page is able to link to them through the navbar
 
 @app.route('/')
 def render_overview():
-    return render_template('home.html')
+    return render_markdown_any('gender_novels_overview', title='Gender in Novels, 1770–1922')
 
 
 @app.route('/copyright.html')
 def render_copyright():
-    return render_template('copyright.html')
-
+    return render_markdown_any('copyright_info')
 
 @app.route('/corpora.html')
 def render_corpora():
@@ -35,16 +34,16 @@ def render_team():
 
 @app.route('/web-scraping.html')
 def render_web_scraping():
-    return render_template('web-scraping.html')
+    return render_markdown_any('corpus_scraping')
 
 
 @app.route('/metadata.html')
 def render_metadata():
-    return render_template('metadata.html')
+    return render_markdown_any('metadata_acquisition')
 
 
-@app.route('/info/<fn>')
-def render_markdown_any(fn):
+@app.route('/markdowns/<fn>')
+def render_markdown_any(fn, title=None):
     import markdown2
     from gender_novels.common import BASE_PATH
     try:
@@ -54,8 +53,9 @@ def render_markdown_any(fn):
         md_in = '**boo**'
     md_in = md_in.replace('(images/', '(/static/markdowns/images/')
     markdown_html = markdown2.markdown(md_in)
-    title_parts = fn.split('_')
-    title = ' '.join([title_word.capitalize() for title_word in title_parts])
+    if title is None:
+        title_parts = fn.split('_')
+        title = ' '.join([title_word.capitalize() for title_word in title_parts])
 
     return render_template('blank_markdown.html', title=title, markdown_html=markdown_html)
 
@@ -64,6 +64,6 @@ def render_markdown_any(fn):
 if __name__ == '__main__':
     # Open a web browser on the landing page
     import webbrowser
-    webbrowser.open('http://127.0.0.1:8021/', new=2)
+    webbrowser.open('http://127.0.0.1:8021/', new=1)
     app.run(host='127.0.0.1', port='8021')
 
